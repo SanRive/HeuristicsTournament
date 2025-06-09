469b6870fcc3b567ff4e1aaf5592c32b605b7558
Santiago Rivera Mejia <sriverame@unal.edu.co>
2025-06-09 14:36:26 -0500

    Update core_de.py

--- core_de.py
+++ core_de.py
@@ -1,9 +1,3 @@
-# -----------------------------
-# Cell 3: 🔒 Core DE Algorithm (Read-only)
-# nbgrader: locked
-# nbgrader: grade
-# nbgrader: points: 0
-# -----------------------------
 def run_de(strategy, func_name, F, CR, seed=0, D=10, NP=30, time_limit=10):
     np.random.seed(seed)
 
